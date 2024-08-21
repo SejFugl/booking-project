9edc85b581590f73b33fb12cd322e4e272ef5a11
SejFugl <erse54105@edu.ucl.dk>
2024-08-21 20:30:59 +0200

    feat: Add App component and related files
    This commit adds the App component along with its related files, including the CSS file and the components it imports. The App component serves as the main entry point for the application and includes the Header, LandingText, and Wave components. This change sets up the basic structure of the application.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import LandingText from "./components/LandingText";
+import Header from "./components/Header";
+import Wave from "./components/Wave";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <LandingText />
+      <Wave />
+    </>
+  );
+}
+
+export default App;
